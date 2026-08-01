2999c9e2c14bb2056503484a137ef8ab3613cab9
krishpatel20217 <krishpatel20217@gmail.com>
2026-08-01 23:36:10 +0530

    Most Common Elements

---
+++ Blind 75/Medium/Most Common Elements/solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} k
+ * @return {number[]}
+ */
+export default function mostCommonElements(nums, k) {
+  const rem = new Map();
+  for(let i=0;i<nums.length;i++){
+    if(rem.has(nums[i])){
+      rem.set(nums[i],rem.get(nums[i])+1)
+    }else{
+      rem.set(nums[i],1)
+    }
+  }
+  const arr = [];
+  for(const [key,value] of rem){
+    arr.push([key,value]);
+  }
+  arr.sort(function(a,b){
+    return b[1] - a[1]
+  })
+  const result=[];
+  for(let i=0;i<k;i++){
+    result.push(arr[i][0]);
+  }
+  return result
+}
